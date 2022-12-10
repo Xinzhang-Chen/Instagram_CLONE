e94f1acd317d703e779659a0e17e5e25a9815830
Johnson-GitHub-New <64173435+Johnson-GitHub-New@users.noreply.github.com>
2022-12-10 12:30:38 +1100

    Initial commit

--- App.js
+++ App.js
@@ -1,20 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+import LandingScreen from './screens/auth/LandingScreen';
+import LoginScreen from './screens/auth/LoginScreen';
+import RegisterScreen from './screens/auth/RegisterScreen';
+
 export default function App() {
+  const stack = createNativeStackNavigator();
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <stack.Navigator initialRouteName="Landing">
+        <stack.Screen name="Landing" component={LandingScreen} options={{ headerShown: false }} />
+        <stack.Screen name="Login" component={LoginScreen} options={{ headerShown: false }} />
+        <stack.Screen name="Register" component={RegisterScreen} options={{ headerShown: false }} />
+      </stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ screens/auth/LandingScreen.jsx
@@ -0,0 +1,17 @@
+import { Text, View, Button } from 'react-native';
+import React from 'react';
+import tw from 'twrnc';
+import { useNavigation } from '@react-navigation/native';
+
+const LandingScreen = () => {
+  const navigate = useNavigation();
+
+  return (
+    <View style={tw`flex-1 justify-center`}>
+      <Button title="Login" onPress={() => navigate.navigate('Login')} />
+      <Button title="Register" onPress={() => navigate.navigate('Register')} />
+    </View>
+  );
+};
+
+export default LandingScreen;
